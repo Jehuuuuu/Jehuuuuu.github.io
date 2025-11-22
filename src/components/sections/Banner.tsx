f7cf21cf4875f7a01edeb34a358362084ff84a65
Jehu Paulo C. Rodriguez <jehurodriguez143@gmail.com>
2025-11-22 16:44:19 +0800

    refactor: add constants folder to cleanup constant code

--- src/components/sections/Banner.tsx
+++ src/components/sections/Banner.tsx
@@ -1,62 +1,10 @@
-
-import { FaCss3Alt, FaGitAlt, FaHtml5, FaPython, FaReact } from "react-icons/fa6";
 import { InfiniteMovingCards } from "../ui/infinite-moving-cards";
-import { SiDjango, SiFlask, SiJavascript, SiTypescript } from "react-icons/si";
-import { RiNextjsFill } from "react-icons/ri";
-import { TbSql } from "react-icons/tb";
+import { techStackItems } from "@/lib/constants/techstack";
 
 export default function Banner() {
-  const frontendStackItems =
-    [
-      {
-        icon: <SiJavascript size={50} />,
-        title: "JavaScript"
-      },
-      {
-        icon: <SiTypescript size={50} />,
-        title: "TypeScript"
-      },
-      {
-        icon: <FaReact size={50} />,
-        title: "React"
-      },
-      {
-        icon: <RiNextjsFill size={50} />,
-        title: "Nextjs"
-      },
-      {
-        icon: <FaHtml5 size={50} />,
-        title: "HTML"
-      },
-      {
-        icon: <FaCss3Alt size={50} />,
-        title: "CSS"
-      },
-      {
-        icon: <FaPython size={50} />,
-        title: "Python"
-      },
-      {
-        icon: <SiDjango size={50} />,
-        title: "Django"
-      },
-      {
-        icon: <SiFlask size={50} />,
-        title: "Flask"
-      },
-      {
-        icon: <TbSql size={50} />,
-        title: "SQL"
-      },
-      {
-        icon: <FaGitAlt size={50} />,
-        title: "Git"
-      },
-    ]
-
   return (
     <div>
-      <InfiniteMovingCards items={frontendStackItems}
+      <InfiniteMovingCards items={techStackItems}
         direction="left"
         speed="slow"
       />
---
+++ src/lib/constants/techstack.tsx
@@ -0,0 +1,55 @@
+import { TbSql } from "react-icons/tb";
+import { SiDjango, SiFlask, SiJavascript, SiTypescript } from "react-icons/si";
+import { RiNextjsFill } from "react-icons/ri";
+import { FaCss3Alt, FaGitAlt, FaHtml5, FaPython, FaReact } from "react-icons/fa6";
+
+export const techStackItems =
+  [
+    {
+      icon: <SiJavascript size={50} />,
+      title: "JavaScript"
+    },
+    {
+      icon: <SiTypescript size={50} />,
+      title: "TypeScript"
+    },
+    {
+      icon: <FaReact size={50} />,
+      title: "React"
+    },
+    {
+      icon: <RiNextjsFill size={50} />,
+      title: "Nextjs"
+    },
+    {
+      icon: <FaHtml5 size={50} />,
+      title: "HTML"
+    },
+    {
+      icon: <FaCss3Alt size={50} />,
+      title: "CSS"
+    },
+    {
+      icon: <FaPython size={50} />,
+      title: "Python"
+    },
+    {
+      icon: <SiDjango size={50} />,
+      title: "Django"
+    },
+    {
+      icon: <SiFlask size={50} />,
+      title: "Flask"
+    },
+    {
+      icon: <TbSql size={50} />,
+      title: "SQL"
+    },
+    {
+      icon: <FaGitAlt size={50} />,
+      title: "Git"
+    },
+  ]
+
+
+
